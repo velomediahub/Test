f825cf5082c6ba7bb15fc2804a57ddbf2f6474ad
velomediahub <velomediahub@users.noreply.github.com>
2025-08-25 03:26:42 -0700

    Initial commit for kids coloring pages website with rainbow and cupcake themes.

---
+++ script.js
@@ -0,0 +1,3 @@
+// Add any interactive JavaScript here if needed
+// For a simple coloring page, this might remain empty or contain basic functionalities
+console.log("Coloring page scripts loaded!");
